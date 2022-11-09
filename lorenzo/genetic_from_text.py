ed8bd94d0c28e9beda1a369ae9271112813912ed
lorenzoleuzzi1 <leuzzi.lorenzo1@gmail.com>
2022-11-09 20:42:56 +0100

    changes and results

--- lorenzo/genetic_from_text.py
+++ lorenzo/genetic_from_text.py
@@ -1,14 +1,14 @@
-from PIL import Image
 import imageio # For gif saving
 from evolutionary_algorithm import ea
 import cv2
 import matplotlib.pyplot as plt
+import os
 
 #Load and show original image for tracking (convert to black and white)
 image_path  = "images/cubismo_picasso15072.jpg"
 
 #Adjust hyperparameters
-NUMBER_OF_GENERATIONS = 1000
+NUMBER_OF_GENERATIONS = 10000
 POPULATION_NUMBER = 50  # How many images in 1 generation (without elitism)
 MUTATION_CHANCE = 0.1  # Chance of mutating (adding random shapes)
 MUTATION_STRENGTH = 1  # How many shapes to add in mutation
@@ -29,16 +29,22 @@ plt.xlabel('generations')
 plt.ylabel('average fitness')
 
 if ELITISM:
-    save_path = "lorenzo/results/gens{}_pop{}_mchance{}_mstrength{}_elit{}".format(
+    save_dir = "psns_gens{}_pop{}_mchance{}_mstrength{}_elit{}".format(
         NUMBER_OF_GENERATIONS, POPULATION_NUMBER,MUTATION_CHANCE,
         MUTATION_STRENGTH,ELITISM_NUMBER
     )
 else:
-    save_path = "lorenzo/results/gens{}_pop{}_mchance{}_mstrength{}".format(
+    save_dir = "psns_gens{}_pop{}_mchance{}_mstrength{}".format(
         NUMBER_OF_GENERATIONS, POPULATION_NUMBER,MUTATION_CHANCE,MUTATION_STRENGTH
     )
-# Save gif, best output and plot
+
+save_path = os.path.join("lorenzo", "results", save_dir)
+
+# Save plot, gif and best output 
+if not os.path.exists(save_path):
+    os.mkdir(save_path)
+plt.savefig(save_path + "/plot.png")
 imageio.mimsave(save_path + "/output.gif", save_gif)
 cv2.imwrite(save_path + "/output_best.jpg", save_gif[-1]) 
-plt.savefig(save_path + "plot.png")
+
  
