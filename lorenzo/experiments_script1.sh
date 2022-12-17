bdbdf0adecba76073d3520b7b8e7c2f5c4dfb07f
lorenzoleuzzi1 <leuzzi.lorenzo1@gmail.com>
2022-12-17 11:03:15 +0100

    results

--- lorenzo/experiments_script1.sh
+++ lorenzo/experiments_script1.sh
@@ -11,3 +11,5 @@
 python3 lorenzo/genetic_from_text.py -d mse  -n_gen 50 -pop_n 50 -m_c 0.1 -m_s 1 -e True -e_n 5
 python3 lorenzo/genetic_from_text.py -d psnr  -n_gen 50 -pop_n 50 -m_c 0.1 -m_s 1 -e True -e_n 5
 
+python3 lorenzo/genetic_from_text.py -d mse  -n_gen 15000 -pop_n 50 -m_c 0.1 -m_s 1 -e True -e_n 5
+
