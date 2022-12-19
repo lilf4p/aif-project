2af31331569bfffd4db79b2798bb3cee9c5505c1
lorenzoleuzzi1 <leuzzi.lorenzo1@gmail.com>
2022-12-19 13:08:06 +0100

    config

--- lorenzo/genetic_from_text.py
+++ lorenzo/genetic_from_text.py
@@ -3,34 +3,32 @@ from evolutionary_algorithm import ea
 import cv2
 import matplotlib.pyplot as plt
 import os
-import argparse
+#import argparse
+import json
 
-#hyperparam tuning
-parser = argparse.ArgumentParser(description='genetic algorithm from text')
-parser.add_argument('-d', '--distance', type=str ,default="ssim", help="psnr, mse")
-parser.add_argument('-n_gen', '--number_of_generations', type=int ,default=7000) 
-parser.add_argument('-pop_n', '--population_number', type=int ,default=50) 
-parser.add_argument('-m_c', '--mutation_change', type=float ,default=0.1)
-parser.add_argument('-m_s', '--mutation_strength', type=int ,default=1)
-parser.add_argument('-e', '--elitism', type=bool ,default=True)
-parser.add_argument('-e_n', '--elitism_number', type=int ,default=4)
+#read config
+with open('lorenzo/config.json') as json_file:
+    data = json.load(json_file)
+    experiments_names = list(data.keys())
 
-args = parser.parse_args()
+print("Available experiments: ")
+for experiment_name in experiments_names:
+    print("{}".format(experiment_name))
+print("Select one: ")
+selected_experiment = str(input())
+config = data[selected_experiment]
 
-#Load and show original image for tracking (convert to black and white)
-image_path  = "images/cubismo_picasso15072.jpg"
-
+IMAGE_PATH = config['image_path']
 #Adjust hyperparameters
-DISTANCE_METRIC = args.distance 
-NUMBER_OF_GENERATIONS = args.number_of_generations
-POPULATION_NUMBER = args.population_number  # How many images in 1 generation (without elitism)
-MUTATION_CHANCE = args.mutation_change  # Chance of mutating (adding random shapes)
-MUTATION_STRENGTH = args.mutation_strength  # How many shapes to add in mutation
-ELITISM = args.elitism  # Turn on/off elitism (transfering best images to next generation without crossover)
-ELITISM_NUMBER = args.elitism_number  # How many best images transfer to next generation (elitism)
+DISTANCE_METRIC = config['distance_metric'] 
+NUMBER_OF_GENERATIONS = config['max_epochs']
+POPULATION_NUMBER = config['population_size']  # How many images in 1 generation (without elitism)
+MUTATION_CHANCE = config['mutation_chance']  # Chance of mutating (adding random shapes)
+MUTATION_STRENGTH = config['mutation_strength']  # How many shapes to add in mutation
+ELITISM = config['elitism']  # Turn on/off elitism (transfering best images to next generation without crossover)
+ELITISM_NUMBER = config['elitism_size']  # How many best images transfer to next generation (elitism)
 
-
-save_gif, stats = ea(image_path, DISTANCE_METRIC, NUMBER_OF_GENERATIONS, POPULATION_NUMBER, MUTATION_CHANCE, 
+save_gif, stats = ea(IMAGE_PATH, DISTANCE_METRIC, NUMBER_OF_GENERATIONS, POPULATION_NUMBER, MUTATION_CHANCE, 
                         MUTATION_STRENGTH,  ELITISM, ELITISM_NUMBER)
 
 
@@ -39,13 +37,13 @@ plt.xlabel('generations')
 plt.ylabel('top fitness')
 
 if ELITISM:
-    save_dir = "{}_gens{}_pop{}_mchance{}_mstrength{}_elit{}".format(
-        DISTANCE_METRIC, NUMBER_OF_GENERATIONS, POPULATION_NUMBER,
+    save_dir = "{}_{}_gens{}_pop{}_mchance{}_mstrength{}_elit{}".format(
+        selected_experiment, DISTANCE_METRIC, NUMBER_OF_GENERATIONS, POPULATION_NUMBER,
         MUTATION_CHANCE,MUTATION_STRENGTH,ELITISM_NUMBER
     )
 else:
-    save_dir = "{}_gens{}_pop{}_mchance{}_mstrength{}".format(
-        DISTANCE_METRIC, NUMBER_OF_GENERATIONS, POPULATION_NUMBER,
+    save_dir = "{}_{}_gens{}_pop{}_mchance{}_mstrength{}".format(
+        selected_experiment, DISTANCE_METRIC, NUMBER_OF_GENERATIONS, POPULATION_NUMBER,
         MUTATION_CHANCE,MUTATION_STRENGTH
     )
 
@@ -63,5 +61,3 @@ if not os.path.exists(save_path):
 plt.savefig(save_path + "/plot.png")
 imageio.mimsave(save_path + "/output.gif", save_gif)
 cv2.imwrite(save_path + "/output_best.jpg", save_gif[-1]) 
-
- 
