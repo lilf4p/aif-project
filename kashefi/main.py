10c18b59640ba86367eb6699ef1cdf83b6f87207
M-Kashefi <m.kashefi@studenti.unipi.it>
2022-12-29 09:32:27 +0100

    CLI Interface added

--- kashefi/main.py
+++ kashefi/main.py
@@ -19,34 +19,22 @@ import elitism_callback
 import matplotlib.pyplot as plt
 import seaborn as sns
 import time
-from scoop import futures
 import multiprocessing
-from multiprocessing import freeze_support
 
+import json
+from config import get_config, get_experiment_names, get_experiment_codes, load_json
 
 start_time = time.time()
 C_METHOD = "MSE"
 
 # problem related constants
 POLYGON_SIZE = 6
-NUM_OF_POLYGONS = 700
-
-# calculate total number of params in chromosome:
-# For each polygon we have:
-# two coordinates per vertex, 3 color values, one alpha value,  (starting and ending angles of arc, pie, ..) or (fixed radius of polygons)
-# NUM_OF_PARAMS = NUM_OF_POLYGONS * (POLYGON_SIZE * 2 + 5)
-NUM_OF_PARAMS = NUM_OF_POLYGONS * (POLYGON_SIZE * 2 + 4)
+NUM_OF_POLYGONS = 200
 
 # Genetic Algorithm constants:
-# POPULATION_SIZE = 200
-# P_CROSSOVER = 0.9  # probability for crossover
-# P_MUTATION = 0.008   # probability for mutating an individual
-# MAX_GENERATIONS = 500
-# HALL_OF_FAME_SIZE = 30
-# CROWDING_FACTOR = 10.0  # crowding factor for crossover and mutation
-POPULATION_SIZE = 800
+POPULATION_SIZE = 300
 P_CROSSOVER = 0.9  # probability for crossover
-P_MUTATION = 0.01   # probability for mutating an individual
+P_MUTATION = 0.1  # probability for mutating an individual
 MAX_GENERATIONS = 2000
 HALL_OF_FAME_SIZE = 20
 CROWDING_FACTOR = 10.0  # crowding factor for crossover and mutation
@@ -57,20 +45,20 @@ random.seed(RANDOM_SEED)
 
 # create the image test class instance:
 imageTest = image_test.ImageTest("images/monalisa.png", POLYGON_SIZE)
-# imageTest = image_test.ImageTest("images/tower.jpg", POLYGON_SIZE)
 
 # calculate total number of params in chromosome:
 # For each polygon we have:
-# two coordinates per vertex, 3 color values, one alpha value, (starting and ending angles of arc, pie, ..) or (fixed radius of polygons)
-# NUM_OF_PARAMS = NUM_OF_POLYGONS * (POLYGON_SIZE * 2 + 5)
-NUM_OF_PARAMS = NUM_OF_POLYGONS * (POLYGON_SIZE * 2 + 4)
-# NUM_OF_PARAMS = NUM_OF_POLYGONS * (POLYGON_SIZE * 2 )
+# two coordinates per vertex, 3 color values, one alpha value, radius of polygon
+NUM_OF_PARAMS = NUM_OF_POLYGONS * (POLYGON_SIZE * 2 + 5)
 
 # all parameter values are bound between 0 and 1, later to be expanded:
 BOUNDS_LOW, BOUNDS_HIGH = 0.0, 1.0  # boundaries for all dimensions
 
-toolbox = base.Toolbox()
+MUTATE_METHODE = "PolynomialBounded"
+MATE_METHODE = "SimulatedBinaryBounded"
+SELECTION_METHOD = "Tournament"
 
+toolbox = base.Toolbox()
 
 # define a single objective, minimizing fitness strategy:
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
@@ -99,7 +87,6 @@ toolbox.register("populationCreator",
                  toolbox.individualCreator)
 
 
-# fitness calculation using MSE as difference metric:
 def getDiff(individual):
     if C_METHOD == "MSE":
         return imageTest.getDifference(individual, "MSE"),
@@ -108,41 +95,179 @@ def getDiff(individual):
     elif C_METHOD == "MSSIM":
         return imageTest.getDifference(individual, "MSSIM"),
 
+# fitness calculation as difference metric:
+
 toolbox.register("evaluate", getDiff)
 
+def setup(experiment,key):
 
-# genetic operators:
-# toolbox.register("select", tools.selBest, fit_attr='fitness')
-toolbox.register("select", tools.selTournament, tournsize=2)
-SELECTION_METHOD = "Tournament"
+    key = (key.split('-'))[0]
+    if (key == "MTSF"):
+        C_METHOD = "MSE"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "FlipBIt"
+    elif (key == "MTSP"):
+        C_METHOD = "MSE"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "MTUP"):
+        C_METHOD = "MSE"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "MTUF"):
+        C_METHOD = "MSE"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "FlipBit"
+    elif (key == "MSSP"):
+        C_METHOD = "MSE"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "MSSF"):
+        C_METHOD = "MSE"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "FlipBit"
+    elif (key == "MSUP"):
+        C_METHOD = "MSE"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "MSUF"):
+        C_METHOD = "MSE"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "FlipBit"
 
-# toolbox.register("mate",tools.cxUniform, indpb=0.5)
-toolbox.register("mate",tools.cxSimulatedBinaryBounded,low=BOUNDS_LOW,up=BOUNDS_HIGH,eta=CROWDING_FACTOR)
-MATE_METHODE = "SimulatedBinaryBounded"
 
-# toolbox.register("mutate",tools.mutFlipBit,indpb=1.0/NUM_OF_PARAMS)
-toolbox.register("mutate",tools.mutPolynomialBounded,low=BOUNDS_LOW,up=BOUNDS_HIGH,eta=CROWDING_FACTOR,indpb=1.0/NUM_OF_PARAMS)
-MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "STSF"):
+        C_METHOD = "SSIM"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "FlipBIt"
+    elif (key == "STSP"):
+        C_METHOD = "SSIM"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "STUP"):
+        C_METHOD = "SSIM"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "STUF"):
+        C_METHOD = "SSIM"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "FlipBit"
+    elif (key == "SSSP"):
+        C_METHOD = "SSIM"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "SSSF"):
+        C_METHOD = "SSIM"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "FlipBit"
+    elif (key == "SSUP"):
+        C_METHOD = "SSIM"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "SSUF"):
+        C_METHOD = "SSIM"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "FlipBit"
 
-# save the best current drawing every 100 generations (used as a callback):
-def saveImage(gen, polygonData):
-    # only every 100 generations:
-    if gen % 100 == 0:
-        # create folder if does not exist:
-        folder = "images/results/run-{}-{}-{}-{}-{}-{}-{}-{}-{}".format(POLYGON_SIZE, NUM_OF_POLYGONS, C_METHOD, POPULATION_SIZE, MAX_GENERATIONS, SELECTION_METHOD, MATE_METHODE, MUTATE_METHODE, start_time)
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        # save the image in the folder:
-        imageTest.saveImage(polygonData,
-                            "{}/after-{}-gen.png".format(folder, gen),
-                            "After {} Generations".format(gen))
-# Genetic Algorithm flow:
-def main():
-    # Concurrent Execution should be enabled
-    # toolbox.register("map", futures.map)
-    # execute the algorithm in multiple threads simultaneously
-    pool = multiprocessing.Pool()
-    toolbox.register("map", pool.map)
+
+    elif (key == "MSTSF"):
+        C_METHOD = "MSSIM"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "FlipBIt"
+    elif (key == "MSTSP"):
+        C_METHOD = "MSSIM"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "MSTUP"):
+        C_METHOD = "MSSIM"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "MSTUF"):
+        C_METHOD = "MSSIM"
+        SELECTION_METHOD = "Tournament"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "FlipBit"
+    elif (key == "MSSSP"):
+        C_METHOD = "MSSIM"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "MSSSF"):
+        C_METHOD = "MSSIM"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "SimulatedBinaryBounded"
+        MUTATE_METHODE = "FlipBit"
+    elif (key == "MSSUP"):
+        C_METHOD = "MSSIM"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "PolynomialBounded"
+    elif (key == "MSSUF"):
+        C_METHOD = "MSSIM"
+        SELECTION_METHOD = "SelectBest"
+        MATE_METHODE = "Uniform"
+        MUTATE_METHODE = "FlipBit"
+
+    # load experiment setup
+    # problem related constants
+    POLYGON_SIZE = experiment["POLYGON_SIZE"]
+    NUM_OF_POLYGONS = experiment["NUM_OF_POLYGONS"]
+
+    # Genetic Algorithm constants:
+    POPULATION_SIZE = experiment["POPULATION_SIZE"]
+    P_CROSSOVER = experiment["P_CROSSOVER"]  # probability for crossover
+    P_MUTATION = experiment['P_MUTATION']  # probability for mutating an individual
+    MAX_GENERATIONS = experiment['MAX_GENERATION']
+    HALL_OF_FAME_SIZE = experiment['HOLL_OF_FAME_SIZE']
+    CROWDING_FACTOR = experiment['CROWDING_FACTOR']  # crowding factor for crossover and mutation
+
+    # set the random seed:
+    RANDOM_SEED = 42
+    random.seed(RANDOM_SEED)
+
+    # create the image test class instance:
+    imageTest = image_test.ImageTest(experiment['IMAGE'], POLYGON_SIZE)
+
+    # calculate total number of params in chromosome:
+    # For each polygon we have:
+    # two coordinates per vertex, 3 color values, one alpha value, radius of polygon
+    NUM_OF_PARAMS = NUM_OF_POLYGONS * (POLYGON_SIZE * 2 + 5)
+
+    # all parameter values are bound between 0 and 1, later to be expanded:
+    BOUNDS_LOW, BOUNDS_HIGH = experiment['BOUNDS_LOW'], experiment['BOUNDS_HIGH']  # boundaries for all dimensions
+
+    #########################################################
+
+
+    #
+    # genetic operators:
+    toolbox.register("select", tools.selBest, fit_attr='fitness') if SELECTION_METHOD == "SelectBest" else toolbox.register("select", tools.selTournament, tournsize=2)
+
+    #
+    toolbox.register("mate", tools.cxSimulatedBinaryBounded, low=BOUNDS_LOW, up=BOUNDS_HIGH, eta=CROWDING_FACTOR) if MATE_METHODE == "SimulatedBinaryBounded" else toolbox.register("mate",tools.cxUniform, indpb=0.5)
+
+    #
+    toolbox.register("mutate", tools.mutPolynomialBounded, low=BOUNDS_LOW, up=BOUNDS_HIGH, eta=CROWDING_FACTOR,
+                     indpb=1.0 / NUM_OF_PARAMS) if MUTATE_METHODE == "PolynomialBounded" else toolbox.register("mutate",tools.mutFlipBit,indpb=1.0/NUM_OF_PARAMS)
 
     # create initial population (generation 0):
     population = toolbox.populationCreator(n=POPULATION_SIZE)
@@ -155,17 +280,16 @@ def main():
     # define the hall-of-fame object:
     hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
 
-
     # perform the Genetic Algorithm flow with elitism and 'saveImage' callback:
     population, logbook = elitism_callback.eaSimpleWithElitismAndCallback(population,
-                                                      toolbox,
-                                                      cxpb=P_CROSSOVER,
-                                                      mutpb=P_MUTATION,
-                                                      ngen=MAX_GENERATIONS,
-                                                      callback=saveImage,
-                                                      stats=stats,
-                                                      halloffame=hof,
-                                                      verbose=True)
+                                                                          toolbox,
+                                                                          cxpb=P_CROSSOVER,
+                                                                          mutpb=P_MUTATION,
+                                                                          ngen=MAX_GENERATIONS,
+                                                                          callback=saveImage,
+                                                                          stats=stats,
+                                                                          halloffame=hof,
+                                                                          verbose=True)
 
     # print best solution found:
     best = hof.items[0]
@@ -182,7 +306,6 @@ def main():
 
     # extract statistics:
     minFitnessValues, meanFitnessValues = logbook.select("min", "avg")
-
 
     # plot statistics:
     sns.set_style("whitegrid")
@@ -203,6 +326,49 @@ def main():
     # save gif
     imageTest.saveGif(folder + "/result.gif")
     np.savetxt(folder + '/evolution_log.csv', logbook, delimiter=',', fmt='%s')
+
+
+# save the best current drawing every 100 generations (used as a callback):
+def saveImage(gen, polygonData):
+    # only every 100 generations:
+    if gen % 100 == 0:
+        # create folder if does not exist:
+        folder = "images/results/run-{}-{}-{}-{}-{}-{}-{}-{}-{}".format(POLYGON_SIZE, NUM_OF_POLYGONS, C_METHOD,
+                                                                            POPULATION_SIZE, MAX_GENERATIONS,
+                                                                            SELECTION_METHOD, MATE_METHODE,
+                                                                            MUTATE_METHODE, start_time)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        # save the image in the folder:
+        imageTest.saveImage(polygonData,
+                            "{}/after-{}-gen.png".format(folder, gen),
+                            "After {} Generations".format(gen))
+
+
+
+# Genetic Algorithm flow:
+def main():
+
+    # Concurrent Execution should be enabled
+    # toolbox.register("map", futures.map)
+    # execute the algorithm in multiple threads simultaneously
+    pool = multiprocessing.Pool()
+    toolbox.register("map", pool.map)
+    sf = open('experiments.json')
+    settings = json.loads(sf.read())
+    experiments = get_experiment_names()
+    for e in experiments:
+        print(e)
+    print("You can add or update experiments on /kashefi/experiments.json file")
+
+    exp = input("Enter a test name to start: ")
+    exp_keys = get_experiment_codes()
+    if(exp == "Exit"):
+        return
+    elif(exp not in exp_keys):
+        main()
+    experiment = get_config(exp)
+    setup(experiment,exp)
 
 
 
