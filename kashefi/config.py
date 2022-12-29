b8d76484961abb7a24a08f98367b763b2983d83d
M-Kashefi <m.kashefi@studenti.unipi.it>
2022-12-29 09:32:27 +0100

    CLI Interface added

---
+++ kashefi/config.py
@@ -0,0 +1,25 @@
+import json
+
+def get_config(experiment_name: str):
+    with open('experiments.json') as json_file:
+        data = json.load(json_file)
+        return data[experiment_name]
+
+def get_experiment_codes():
+    with open('experiments.json') as json_file:
+        data = json.load(json_file)
+        return list(data.keys())
+def get_experiment_names():
+    with open('experiments.json') as json_file:
+        data = json.load(json_file)
+        ex_names  = list()
+        keys = list(data.keys())
+        for k in keys:
+            ex_names.append(data[k]['TEST'])
+        return list(ex_names)
+
+#function to load a json and return a dictionary
+def load_json(file):
+    with open(file) as json_file:
+        data = json.load(json_file)
+        return data
--- kashefi/image_test.py
+++ kashefi/image_test.py
@@ -8,9 +8,9 @@ from skimage.metrics import structural_similarity
 import cv2
 import matplotlib.pyplot as plt
 
-MAX_STEPS = 200
-FLAG_LOCATION = 0.5
-log_dump = []
+# MAX_STEPS = 200
+# FLAG_LOCATION = 0.5
+# log_dump = []
 
 class ImageTest:
     def __init__(self, imagePath, polygonSize):
@@ -38,60 +38,31 @@ class ImageTest:
         :return: the image containing the polygons (Pillow format)
         """
 
-        # start with a new image:
+        # start with a new image: gray background (average starting value)
         image = Image.new('RGB', (self.width, self.height),(127,127,127,127))#TODO
         draw = ImageDraw.Draw(image, 'RGBA')
-        # image = Image.new('L', (self.width, self.height), 127)#TODO
-        # draw = ImageDraw.Draw(image, 'L')
 
         # divide the polygonData to chunks, each containing the data for a single polygon:
-        chunkSize = self.polygonSize * 2 + 4 # (x,y) per vertex + (RGBA) default
-        # chunkSize = self.polygonSize * 2 + 5 # (x,y,r) per vertex + (RGBA)
-        polygons = self.list2Chunks(polygonData, c6hunkSize)
-        radius = self.width/20 if self.width < self.height else self.height/20
-        # max_radius = self.width/20 if self.width < self.height else self.height/20
+        chunkSize = self.polygonSize * 2 + 5 # (x,y) per vertex + (RGBA) default + polygon radius
+        polygons = self.list2Chunks(polygonData, chunkSize)
+        #making radius smaller would start slower convergence but better results in next generations
+        max_radius = self.width/12 if self.width < self.height else self.height/12
         # iterate over all polygons and draw each of them into the image:
         for poly in polygons:
             index = 0
             # calc the fixed radius for all vertices of polygon
-            # radius = int((poly[self.polygonSize * 2 + 4] * max_radius) % max_radius)
+            radius = int((poly[self.polygonSize * 2 + 4] * max_radius) % max_radius)
             # extract the vertices of the current polygon:
             vertices = []
             for vertex in range(self.polygonSize):
                 vertices.append((int(poly[index] * self.width), int(poly[index + 1] * self.height), radius if radius > 0 else 1)) #default
                 index += 2
-            # r = (r/2 if r > self.width/2 else r)
-            # vertices.append((int(poly[index] * self.width), int(poly[index + 1] * self.height), r))
-
-                # index += 2 #default
-
             # extract the RGB and alpha values of the current polygon:
             red = int(poly[index] * 255) #default
             green = int(poly[index + 1] * 255) #default
             blue = int(poly[index + 2] * 255) #default
             alpha = int(poly[index + 3] * 255) #default
-            # starting_angle = int(poly[index + 4] * 255)
-            # ending_angle = int(poly[index + 5] * 255)
-            # gray = int(poly[index] * 255)
-            # color = int(poly[index] * 255)
-
-            # draw the polygon into the image:
-            # draw.polygon(vertices, (red, green, blue, alpha))
             draw.regular_polygon(vertices[0], self.polygonSize, 0, (red, green, blue, alpha))
-            # draw.regular_polygon(vertices[0], self.polygonSize, 0, gray)
-            # start, end = self.get_random_angles()
-            # start, end = 0, 90
-            # arc_width = int(poly[index] * self.width) % self.width
-            # line_width = 1
-            # radios = int(poly[index] * self.width) % self.width
-            # draw.arc(vertices, start, end, (red, green, blue, alpha), line_width)
-            # draw.pieslice(vertices, starting_angle, ending_angle, (red, green, blue, alpha))
-            # draw.rounded_rectangle(vertices, radios, (red, green, blue, alpha), line_width)
-            # draw.arc(vertices, start, end, (gray, gray, gray, alpha), line_width)
-            # draw.arc(vertices, start, end, 127, line_width)
-            # draw.chord(vertices, start, end, (red, green, blue, alpha), line_width)
-            # draw.polygon(vertices, (gray, gray, gray, 255))
-            # draw.polygon(vertices, 0)
 
         # cleanup:
         del draw
